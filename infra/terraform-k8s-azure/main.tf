e669f6837f4a1d7ecdf2062e16561ea31b77ede3
Gabriel Martins <gabtec@sapo.pt>
2023-04-12 14:11:09 +0100

    ci: update terraform providers

---
+++ infra/terraform-k8s-azure/main.tf
@@ -0,0 +1,25 @@
+# --------------------------------
+# Providers and Versions
+# --------------------------------
+terraform {
+  # terraform version
+  required_version = ">= 1.4.0"
+  # required_version = "1.4.4"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.51.0"
+      # version = "2.66.0"
+    }
+  }
+
+
+}
+
+# --------------------------------
+# Azure
+# --------------------------------
+provider "azurerm" {
+  features {}
+}
