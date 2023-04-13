3fd72ebbf4982e46f19922449a830a85bcb7a0bc
Gabriel Martins <gabtec@sapo.pt>
2023-04-13 14:55:46 +0100

    ci(tf): add aks resource

--- infra/terraform-k8s-azure/aks-cluster.tf
+++ infra/terraform-k8s-azure/aks-cluster.tf
@@ -19,12 +19,12 @@ resource "azurerm_kubernetes_cluster" "default" {
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${var.PROJ_OWNER}-k8s"
-  kubernetes_version = "1.24.10"
+  kubernetes_version  = "1.24.10"
 
   default_node_pool {
-    name            = "default"
-    node_count      = 2
-    vm_size         = "standard_a2_v2" # 2 cpu/ 4 GB
+    name       = "default"
+    node_count = 2
+    vm_size    = "standard_a2_v2" # 2 cpu/ 4 GB
     # vm_size         = "standard_ds2_v2" # 2 cpu/ 7 GB
     # os_disk_size_gb = 30
   }
