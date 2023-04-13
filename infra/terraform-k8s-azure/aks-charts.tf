c0d997b15befd773b5cbb58a91d0b27a46c33b22
Gabriel Martins <gabtec@sapo.pt>
2023-04-13 17:56:44 +0100

    ci: update kub provider

--- infra/terraform-k8s-azure/aks-charts.tf
+++ infra/terraform-k8s-azure/aks-charts.tf
@@ -1,10 +1,10 @@
 provider "kubernetes" {
-  host = azurerm_kubernetes_cluster.default.kube_config.0.host
-  # username               = azurerm_kubernetes_cluster.default.kube_config.0.username
-  # password               = azurerm_kubernetes_cluster.default.kube_config.0.password
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+  host = azurerm_kubernetes_cluster.default.kube_config[0].host
+  # username               = azurerm_kubernetes_cluster.default.kube_config[0].username
+  # password               = azurerm_kubernetes_cluster.default.kube_config[0].password
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config[0].cluster_ca_certificate)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
